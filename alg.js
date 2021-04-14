c8a2c1459e33c8f2ec1720e6a3477be2b353f35b
leonelsosav <ernesto.leonel.vera.sosa@gmail.com>
2021-04-14 18:44:18 -0500

    AG

--- alg.js
+++ alg.js
@@ -1,25 +1,81 @@
-function calcFitness(){
+
+function calcFitness() {
+    var recordActual = Infinity;
     for (let i = 0; i < poblacion.length; i++) {
         var d = calcDistancia(ciudades, poblacion[i]);
-        if(d < record) {
-          record = d;
-          mejor = poblacion[i];
+        //Si la distancia actual es mejor que el record, actualizar record y el mejor
+        if (d < record) {
+            record = d;
+            mejor = poblacion[i];
+            document.getElementById("mejorOrden").innerHTML = `Mejor orden: ${mejor.join(",")}`;
         }
-        fitness[i] = 1/(d+1);
-      }
+        if (d < recordActual) {
+            recordActual = d;
+            mejorActual = poblacion[i];
+        }
+        fitness[i] = 1 / (d + 1);
+    }
 }
-
-function normalizarFitness(){
+//Poner todos los valores entre 0 y 1
+function normalizarFitness() {
     var sum = 0;
-    for (let i = 0; i < poblacion.length; i++) {
+    for (let i = 0; i < fitness.length; i++) {
         sum += fitness[i];
     }
-    for (let i = 0; i < poblacion.length; i++) {
+    for (let i = 0; i < fitness.length; i++) {
         fitness[i] = fitness[i] / sum;
     }
 }
-
-function siguienteGeneracion(){
+//Generar nueva poblacion
+function siguienteGeneracion() {
     var poblacionNueva = [];
-    
+    for (let i = 0; i < poblacion.length; i++) {
+        var ordenA = escogerUno(poblacion, fitness); 
+        var ordenB = escogerUno(poblacion, fitness); //obtener una muestra de la poblacion
+        var orden = crossOver(ordenA, ordenB); 
+        mutar(orden, 0.01);//mutacion aleatoria
+        poblacionNueva[i] = orden;
+    }
+    if (flag < 1) {
+        flag++;
+        console.log(poblacion);
+        console.log(poblacionNueva);
+    }
+    poblacion = poblacionNueva;//actualizar la poblacion
+}
+//escoger un valor dentro de la poblacion que es donde la suma del fitness es menor a 0
+function escogerUno(list, probs) {
+    var idx = 0;
+    var r = random(1);
+
+    while (r > 0) {
+        r -= probs[idx];
+        idx++;
+    }
+    idx--;
+    return list[idx].slice();
+}
+//Crear la parte de crossing entre 2 elementos de la poblacion
+function crossOver(ordenA, ordenB){
+    var inicio = floor(random(ordenA.length-1));
+    var fin = floor(random(inicio+1, ordenA.length))
+    var nuevoOrden = ordenA.slice(inicio, fin);
+
+    for (let i = 0; i < ordenB.length; i++) {
+        if(!nuevoOrden.includes(ordenB[i])){
+            nuevoOrden.push(ordenB[i]);
+        }
+    }
+    return nuevoOrden;
+}
+
+//Mutacion: cambiar 2 valores dentro de un orden(elemento dentro de la poblacion)
+function mutar(orden, velMutacion) {
+    for (let i = 0; i < totalCiudades; i++) {
+        if (random(1) < velMutacion) {
+            var idxA = floor(random(orden.length));
+            var idxB = floor(random(orden.length));
+            intercambiar(orden, idxA, idxB);
+        }
+    }
 }
--- sketch.js
+++ sketch.js
@@ -1,65 +1,80 @@
 var ciudades = [];
 var totalCiudades = 10;
+var tamañoPoblacion = 5;
 var poblacion = [];
 var fitness = [];
 var record = Infinity;
 var mejor;
+var mejorActual;
+var flag = 0;
 
 function setup() {
   createCanvas(600, 600);
   var orden = [];
+  //Se genera las posiciones de las diferentes "ciudades de manera aleatoria" 
   for (let i = 0; i < totalCiudades; i++) {
     var v = createVector(random(width), random(height / 2));
     ciudades.push(v);
     orden.push(i);
   }
-
-  for (let i = 0; i < 10; i++) {
+  //Se crea la poblacion inicial con orden aleatorio
+  for (let i = 0; i < tamañoPoblacion; i++) {
     poblacion.push(shuffle(orden));
   }
-  
-
-  
+  console.log(poblacion);
 }
 
 function draw() {
   background(0);
 
-  //GA
+  //Algoritmo Genetico
   calcFitness();
   normalizarFitness();
   siguienteGeneracion();
 
+  stroke(255,0,0);
+  strokeWeight(1);
+  noFill();
+  beginShape();
+  textSize(32);
+  for (let i = 0; i < ciudades.length; i++) {
+    text(i.toString(),ciudades[i].x, ciudades[i].y < 32 ? ciudades[i].y + 10: ciudades[i].y -10)
+  }
+  //Dibujar la mejor opcion
   stroke(255);
   strokeWeight(4);
   noFill();
   beginShape();
   for (var i = 0; i < mejor.length; i++) {
     var n = mejor[i];
+    strokeWeight(4);
     vertex(ciudades[n].x, ciudades[n].y);
     ellipse(ciudades[n].x, ciudades[n].y, 16, 16);
   }
   endShape();
 
+  //Dibujar la opcion actual
   translate(0, height / 2);
   stroke(255);
   strokeWeight(1);
   noFill();
   beginShape();
-  for (var i = 0; i < mejor.length; i++) {
-    var n = mejor[i];
+  for (var i = 0; i < mejorActual.length; i++) {
+    var n = mejorActual[i];
     vertex(ciudades[n].x, ciudades[n].y);
     ellipse(ciudades[n].x, ciudades[n].y, 8, 8);
   }
   endShape();
 }
 
+//Intercambiar dos valores dentro de un arreglo
 function intercambiar(arr, a, b){
   var temp = arr[a];
   arr[a] = arr[b];
   arr[b] = temp;
 }
 
+//Calcular la distancia entre las ciudades segun un orden (el de la poblacion)
 function calcDistancia(puntos, orden){
   var sum = 0;
   for (let i = 0; i < orden.length-1; i++) {
